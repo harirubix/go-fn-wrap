696cecded4b0aa063110ae2c95b84fadf1b0cbb2
harirubix <hari@rubix.net>
2023-07-05 09:30:31 +0530

    pass string to memory

--- gowasm/main.go
+++ gowasm/main.go
@@ -1,44 +1,119 @@
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/bytecodealliance/wasmtime-go"
+// )
+
+// func add(a, b int32) int32 {
+// 	fmt.Println("Add from Go Wasmtime!")
+// 	fmt.Printf("a: %d, b: %d\n", a, b)
+// 	return a + b
+// }
+
+// func main() {
+// 	// Create an engine and store
+// 	engine := wasmtime.NewEngine()
+// 	store := wasmtime.NewStore(engine)
+
+// 	// Load the WebAssembly module
+// 	module, err := wasmtime.NewModuleFromFile(store.Engine,
+// 		"../target/wasm32-unknown-unknown/debug/callback.wasm",
+// 	)
+// 	if err != nil {
+// 		fmt.Print("Error 0")
+// 		panic(err)
+// 	}
+
+// 	linker := wasmtime.NewLinker(store.Engine)
+// 	linker.DefineFunc(store, "env", "add", add)
+
+// 	instance, err := linker.Instantiate(store, module)
+
+// 	if err != nil {
+// 		fmt.Print("Error 1")
+// 		panic(err)
+// 	}
+
+// 	addAndMultiply := instance.GetExport(store, "add_and_multiply")
+
+// 	// Call the "add_and_multiply" function from Rust
+// 	result, _ := addAndMultiply.Func().Call(store, 2, 3)
+// 	fmt.Println("Result:", result.(int32))
+// }
+
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
+	"os"
 
 	"github.com/bytecodealliance/wasmtime-go"
 )
 
-func add(a, b int32) int32 {
-	fmt.Println("Add from Go Wasmtime!")
-	fmt.Printf("a: %d, b: %d\n", a, b)
-	return a + b
+type WasmtimeRuntime struct {
+	store   *wasmtime.Store
+	memory  *wasmtime.Memory
+	handler *wasmtime.Func
+
+	input  []byte
+	output []byte
+}
+
+func (r *WasmtimeRuntime) Init(wasmFile string) {
+	engine := wasmtime.NewEngine()
+	linker := wasmtime.NewLinker(engine)
+	linker.DefineWasi()
+	wasiConfig := wasmtime.NewWasiConfig()
+	r.store = wasmtime.NewStore(engine)
+	r.store.SetWasi(wasiConfig)
+	linker.FuncWrap("env", "load_input", r.loadInput)
+	linker.FuncWrap("env", "dump_output", r.dumpOutput)
+	wasmBytes, _ := os.ReadFile(wasmFile)
+	module, _ := wasmtime.NewModule(r.store.Engine, wasmBytes)
+	instance, _ := linker.Instantiate(r.store, module)
+	r.memory = instance.GetExport(r.store, "memory").Memory()
+	r.handler = instance.GetFunc(r.store, "handler")
+}
+
+func (r *WasmtimeRuntime) loadInput(pointer int32) {
+	copy(r.memory.UnsafeData(r.store)[pointer:pointer+int32(len(r.input))], r.input)
+}
+
+func (r *WasmtimeRuntime) dumpOutput(pointer int32, muliply int32, length int32) {
+	fmt.Println("multiply :", muliply)
+	r.output = make([]byte, length)
+	copy(r.output, r.memory.UnsafeData(r.store)[pointer:pointer+length])
+}
+
+func (r *WasmtimeRuntime) RunHandler(data []byte, buf int32, b1 int32, b2 int32) []byte {
+	r.input = data
+	r.handler.Call(r.store, buf, b1, b2)
+	fmt.Println("Result:", r.output)
+	return r.output
 }
 
 func main() {
-	// Create an engine and store
-	engine := wasmtime.NewEngine()
-	store := wasmtime.NewStore(engine)
-
-	// Load the WebAssembly module
-	module, err := wasmtime.NewModuleFromFile(store.Engine,
-		"../target/wasm32-unknown-unknown/debug/callback.wasm",
-	)
-	if err != nil {
-		fmt.Print("Error 0")
-		panic(err)
+	size := 16
+	buf := make([]byte, size)
+	for i := 0; i < size; i++ {
+		buf[i] = 'a'
 	}
+	b1 := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b1, 12)
+	fmt.Println(b1)
 
-	linker := wasmtime.NewLinker(store.Engine)
-	linker.DefineFunc(store, "env", "add", add)
+	b2 := make([]byte, 4)
+	binary.LittleEndian.PutUint32(b2, 96)
+	fmt.Println(b2)
 
-	instance, err := linker.Instantiate(store, module)
+	merge1 := append(buf, b1...)
+	merge := append(merge1, b2...)
+	fmt.Println(merge)
 
-	if err != nil {
-		fmt.Print("Error 1")
-		panic(err)
-	}
-
-	addAndMultiply := instance.GetExport(store, "add_and_multiply")
-
-	// Call the "add_and_multiply" function from Rust
-	result, _ := addAndMultiply.Func().Call(store, 2, 3)
-	fmt.Println("Result:", result.(int32))
+	runtime := &WasmtimeRuntime{}
+	runtime.Init("../target/wasm32-unknown-unknown/debug/callback.wasm")
+	runtime.RunHandler(merge, int32(len(buf)), int32(len(b1)), int32(len(b2)))
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,52 @@ pub extern "C" fn multiply(a: i32, b: i32) -> i32 {
     a * b
 }
 
-#[no_mangle]
-pub extern "C" fn add_and_multiply(a: i32, b: i32) -> i32 {
-    let go_add_result = unsafe { add(a, b) };
-    multiply(go_add_result, b)
-}
+// #[no_mangle]
+// pub extern "C" fn add_and_multiply(a: i32, b: i32) -> i32 {
+//     let go_add_result = unsafe { add(a, b) };
+//     multiply(go_add_result, b)
+// }
+
+// extern "C" {
+//     fn add(a: i32, b: i32) -> i32;
+// }
 
 extern "C" {
-    fn add(a: i32, b: i32) -> i32;
+    fn load_input(pointer: *mut u8);
+    fn dump_output(pointer: *const u8, multiply: u32, length: usize);
+}
+
+#[no_mangle]
+pub extern "C" fn handler(input_length: usize , b1_length: usize , b2_length: usize) {
+    // load input data
+    let mut input = Vec::with_capacity(input_length + b1_length + b2_length);
+    // let mut buf: Vec<u8> = Vec::with_capacity(input_length);
+    // let mut b1: Vec<u8> = Vec::with_capacity(b1_length);
+    // let mut b2: Vec<u8> = Vec::with_capacity(b2_length);
+    
+    unsafe {
+        load_input(input.as_mut_ptr());
+        input.set_len(input_length + b1_length + b2_length);
+    }
+
+
+    let (buf, b1_b2) = input.split_at(input_length);
+    let (b1, b2) = b1_b2.split_at(b1_length);
+    // process app data
+    let output = buf.to_ascii_uppercase();
+    // let b1_out: Vec<u8> = b1.to_ascii_uppercase();
+    // let b2_out: Vec<u8> = b2.to_ascii_uppercase();
+
+    let first = u32::from_ne_bytes(b1[0..b1_length].try_into().unwrap());
+    let second = u32::from_ne_bytes(b2[0..b2_length].try_into().unwrap());
+
+    let multiply = first * second;
+
+    // dump output data
+    unsafe {
+        dump_output(output.as_ptr() , multiply , output.len());
+        // dump_output(output.as_ptr(), output.len());
+        // dump_output(b1_out.as_ptr(), b1.len());
+
+    }
 }
